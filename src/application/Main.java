92312be5d7fcb8e87a65f7b374c8db26f6e17846
Gianluca Rea <reagianluca97@gmail.com>
2019-11-20 10:32:18 +0100

    Doc by Daniele

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,9 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("EcoToll");
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- src/application/front/controller/AdminPageController.java
+++ src/application/front/controller/AdminPageController.java
@@ -1,6 +1,52 @@
 package application.front.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class AdminPageController {
 
+    @FXML
+    private ChoiceBox<?> selectMotorway;
+
+    @FXML
+    private TextField showTariffa;
+
+    @FXML
+    private ListView<?> showCaselloList;
+
+    @FXML
+    private Button goAutostradasetting;
+
+    @FXML
+    private ChoiceBox<?> selectCasello;
+
+    @FXML
+    private TextField showKilometro;
+
+    @FXML
+    private Button goCasellosetting;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    void dologout(MouseEvent event) {
+
+    }
+
+    @FXML
+    void gotoAutostradaSetting(MouseEvent event) {
+
+    }
+
+    @FXML
+    void gotoCaselloSetting(MouseEvent event) {
+
+    }
 
 }
+
